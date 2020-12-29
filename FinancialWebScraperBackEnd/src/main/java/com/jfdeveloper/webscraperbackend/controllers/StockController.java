f08a3c3dcf6959ab4562e4fe50fb8cfd4c8a11f5
jfdoyle3 <indellibleink@gmail.com>
2020-12-29 07:28:21 -0500

    added cross-origin to controllers

--- FinancialWebScraperBackEnd/src/main/java/com/jfdeveloper/webscraperbackend/controllers/StockController.java
+++ FinancialWebScraperBackEnd/src/main/java/com/jfdeveloper/webscraperbackend/controllers/StockController.java
@@ -1,9 +1,13 @@
 package com.jfdeveloper.webscraperbackend.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class StockController {
 	
 	@GetMapping(path = "/stocks")

892e2e4d1f4e3cd39191f3d67dd2f26beb9c1180
Jim Doyle <indellibleink@gmail.com>
2020-12-30 01:15:08 -0500

    moving code into packages setting up entity

--- FinancialWebScraperBackEnd/src/main/java/com/jfdeveloper/webscraperbackend/controllers/StockController.java
+++ FinancialWebScraperBackEnd/src/main/java/com/jfdeveloper/webscraperbackend/controllers/StockController.java
@@ -1,18 +1,29 @@
 package com.jfdeveloper.webscraperbackend.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jfdeveloper.webscraperbackend.services.StockServices;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class StockController {
 	
+	@Autowired
+	private StockServices stockServices;
+	
+	
 	@GetMapping(path = "/stocks")
 	public String findAll() {
 		return "Stock Controller";
 	}
+	
+	@GetMapping(path="/testServices")
+		public Object StockService() {
+			return stockServices.displayStock().toString();
+		}
+	
 
 }

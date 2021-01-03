265dbb53599b4b1c8f3ff9189ae4d14eaa041c77
Jim Doyle <indellibleink@gmail.com>
2021-01-03 18:02:42 -0500

    Scraper runs and outputs stock list in json

--- FinancialWebScraperBackEnd/src/main/java/com/jfdeveloper/webscraperbackend/controllers/StockController.java
+++ FinancialWebScraperBackEnd/src/main/java/com/jfdeveloper/webscraperbackend/controllers/StockController.java
@@ -1,18 +1,21 @@
 package com.jfdeveloper.webscraperbackend.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jfdeveloper.webscraperbackend.entity.objects.Stock;
 import com.jfdeveloper.webscraperbackend.services.StockServices;
 import com.jfdeveloper.webscraperbackend.web_scraper.Login;
+import com.jfdeveloper.webscraperbackend.web_scraper.StockList;
 
 
 @RestController
-// @CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StockController {
 	
 	//@Autowired
@@ -20,14 +23,11 @@ public class StockController {
 	
 	
 	@GetMapping(path = "/stocks")
-	public void findAll() throws InterruptedException, IOException {
-		Login.signIn();
+	public  ArrayList<Stock> findAll() throws InterruptedException, IOException {
+		var stocks=Login.signIn();
+		return stocks;
 	}
 	
-//	@GetMapping(path="/testServices")
-//		public Object StockService() {
-//			return stockServices.displayStock().toString();
-//		}
 	
 
 }
